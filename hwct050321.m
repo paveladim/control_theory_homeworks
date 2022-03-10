ed3b104d2229a230015d6395146b634204f53251
pavel petrov <paveladim@yandex.ru>
2022-03-10 21:07:44 +0300

    10.03 from classwork

--- hwct050321.m
+++ hwct050321.m
@@ -8,7 +8,7 @@ function hwct050321
     g  = 9.81;  % коэф. свободного падения
 
     % определим матрицы системы
-    A0 = [m + M , -m * l; 
+    A0 = [m + M, -m * l; 
           -m * l, I + m * l^2];
 
     A1 = diag([Bc, Bp]);
@@ -19,27 +19,23 @@ function hwct050321
     A = [zeros(2, 2), eye(2); -inv(A0) * A2, -inv(A0) * A1];
 
     h = 0.1;
-    Ad = exp(A * h);
-    f = @(s)(exp(A * s) * B);
+    Ad = expm(A * h);
+    f = @(s)(expm(A * s) * B);
 
-    Bd = [0; 0; 0; 0];
-    step = h / 10000;
-    for i = 1:10000
-        Bd = Bd + f(step * (i - 1)) * step;
-    end
+    Bd = integral(f, 0, h, "ArrayValued", true);
     
-    theta = -place(A, B, [-0.1, -0.2, 0.2, 0.1]);
-    
-    nx = size(A, 1); % количество строчек
+    theta = -place(Ad, Bd, [-0.1, -0.2, 0.2, 0.1]);
+
+    nx = size(Ad, 1); % количество строчек
     % задаем время отрисовки графиков
     TIME = 10.0;
     % задаем параметры метода решения системы дифференциальных уравнений
     options = odeset('RelTol', 1e-5, 'AbsTol', 1e-5 * ones(1, nx));
     % задаем начальные условия 
-    x0 = zeros(nx, 1);   
+    x0 = zeros(1, nx);   
 
-    x0(1) = 0.5;
-    x0(2) = -0.4;
+    x0(1) = 0.0;
+    x0(2) = -0.3;
 
     discr = 0 : h : TIME;
     tlst = [];
@@ -47,10 +43,10 @@ function hwct050321
 
     for i = 1 : size(discr, 2) - 1
         ticks = discr(i) : 0.001 : discr(i + 1);
-        [TL, YL] = ode45(@(t, X)(A * X + B * theta * x0), ticks, x0', options);
+        [TL, YL] = ode45(@(t, X)(A * X + B * theta * x0'), ticks, x0, options);
         tlst = [tlst; TL];
         xlst = [xlst; YL];
-        x0 = YL(end, :)';
+        x0 = YL(end, :);
     end
 
     fhandle = figure;
